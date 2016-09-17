e7c529021a3dfc5c50d5958035b12bc83c93308f
lvl4fi4 <lvl4fi4@users.noreply.github.com>
2016-09-17 08:11:03 -0700

    Add files via upload

---
+++ mydll/mydll/GBox.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mydll
+{
+    public partial class GBox : UserControl
+    {
+        public GBox()
+        {
+            InitializeComponent();
+        }
+
+        public UserControl setting(string s, string d) {
+            textBox1.Text = s;
+            textBox2.Text = d;
+            return this;
+        }
+    }
+}
